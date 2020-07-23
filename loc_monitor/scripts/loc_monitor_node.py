851c9d9d5ab2f928fdc10c47b9aac874b52b44e5
Felipe Rivas <rivascf@gmail.com>
2020-07-23 23:02:43 +0000

    Fix range variable value not show

--- loc_monitor/scripts/loc_monitor_node.py
+++ loc_monitor/scripts/loc_monitor_node.py
@@ -2,6 +2,7 @@
 
 import rospy
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
 
 def callback(msg):
     x = msg.pose.pose.position.x
@@ -13,9 +14,16 @@ def callback(msg):
 
     rospy.loginfo('x: {} y: {} o: ({}, {}, {}, {}) '.format(x, y, ox, oy, oz, ow))
 
+def sensorcallback(msg):
+    rmin = msg.range_min
+    rmax = msg.range_max
+    s = msg.ranges
+    rospy.loginfo('rmin: {} rmax: {} i: {}'.format(rmin, rmax, s[len(s)/2]))
+
 def main():
     rospy.init_node('loc_monitor')
     rospy.Subscriber('/odom', Odometry, callback)
+    rospy.Subscriber('/kobuki/laser/scan', LaserScan, sensorcallback)
     rospy.spin()
 
 if __name__ == '__main__':
