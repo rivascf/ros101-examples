c9e26d61eaf8e214a93972014e4a98030fccbd36
Felipe Rivas <rivascf@gmail.com>
2020-07-28 22:38:43 +0000

    Create nav_controller program

---
+++ loc_monitor/scripts/nav_controller.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist, Point
+from tf.transformations import euler_from_quaternion
+from math import atan2
+
+x = 0.0  #metros
+y = 0.0  #metros
+theta = 0.0 #Radianes
+
+def controller_callback(msg):
+    global x
+    global y
+    global theta
+
+    x = msg.pose.pose.position.x
+    y = msg.pose.pose.position.y
+    oq = msg.pose.pose.orientation
+
+    #formato de quaterniones (w, x, y, z) -> (x, y, z, w)
+    (roll, pitch, theta) = euler_from_quaternion([oq.x, oq.y, oq.z, oq.w])
+
+def main():
+    rospy.init_node('nav_controller')
+    
+    sub = rospy.Subscriber('/odom', Odometry, controller_callback)
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+    goal = Point()
+    goal.x = 5
+    goal.y = 5
+
+    speed = Twist()
+    r = rospy.Rate(4)
+
+    while not rospy.is_shutdown():
+        dx = goal.x - x
+        dy = goal.y - y
+        alpha = atan2(dy, dx)
+        speed.linear.x = 0.0
+        speed.angular.z = 0.0
+        rospy.loginfo('dx: {} dy:{} alpha: {}'.format(dx, dy, alpha))
+
+        if abs(alpha - theta) > 0.1:
+            speed.linear.x = 0.0
+            speed.angular.z = 0.3 # rad/s
+        else:
+            speed.linear.x = 0.5 # m/s
+            speed.angular.z = 0.0
+        
+        pub.publish(speed)
+        r.sleep()
+
+if __name__ == '__main__':
+    main()
